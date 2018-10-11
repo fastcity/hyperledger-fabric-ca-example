1b7fda266396ecedd93d725d424dfbb205ac2942
Jiangfei <goujiangfei@blackpai.com>
2018-10-11 14:32:16 +0800

    一些提示

--- cacopy.sh
+++ cacopy.sh
@@ -13,20 +13,21 @@ SDIR=$(
 HOME_DIR=$SDIR/fabric-ca-files
 
 function copyorderer() {
-	echo "======================copyorderer=============================="
+	echo "======================copyorderer start=============================="
 	cp -rf $HOME_DIR/fp.com/admin/* $SDIR/crypto-config/ordererOrganizations/fp.com/users/Admin@fp.com
 	cp -rf $HOME_DIR/fp.com/orderer/* $SDIR/crypto-config/ordererOrganizations/fp.com/orderers/orderer.fp.com
 	cp -rf $HOME_DIR/fp.com/msp/* $SDIR/crypto-config/ordererOrganizations/fp.com/msp
+	echo "======================copyorderer down=============================="
 }
 
 function copypeer() {
-	echo "======================copypeer================================="
+	echo "======================copypeer start================================="
 	cp -rf $HOME_DIR/org1.fp.com/admin/* $SDIR/crypto-config/peerOrganizations/org1.fp.com/users/Admin@org1.fp.com
 	cp -rf $HOME_DIR/org1.fp.com/peer0/* $SDIR/crypto-config/peerOrganizations/org1.fp.com/peers/peer0.org1.fp.com
 	cp -rf $HOME_DIR/org1.fp.com/peer1/* $SDIR/crypto-config/peerOrganizations/org1.fp.com/peers/peer1.org1.fp.com
 	cp -rf $HOME_DIR/org1.fp.com/peer2/* $SDIR/crypto-config/peerOrganizations/org1.fp.com/peers/peer2.org1.fp.com
 	cp -rf $HOME_DIR/org1.fp.com/msp/* $SDIR/crypto-config/peerOrganizations/org1.fp.com/msp
-
+	echo "======================copyorderer down=============================="
 }
 
 function control() {
